967c5449adfaf872dec24a4c279ac6be24e8a48d
Andrew <andrew.chen986@gmail.com>
2022-10-17 23:04:58 -0500

    Display modal on victory

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,13 @@
 const gameBoard = (() => {
     let board = new Array(3);
+    let gameOver = false;
     for (let i = 0; i < board.length; i++) {
         board[i] = new Array(3).fill('');
     }
     function isFull() {
         for (let i = 0; i < board.length; i++) {
             for (let j = 0; j < board[i].length; j++) {
-                if (!board[i][j]) {
+                if (board[i][j] === '') {
                     return false;
                 }
             }
@@ -23,7 +24,36 @@ const gameBoard = (() => {
         alert("Click an empty square!");
         return false;
     }
-    return {board, isFull, markBoard}
+    function victory() {
+        // check horizontals
+        for (let i = 0; i < board.length; i++) {
+            if (board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+                gameOver = true;
+                return gameOver;
+            }
+        }
+        
+        // check verticals
+        for (let j = 0; j < board[0].length; j++) {
+            if (board[0][j] === board[1][j] === board[2][j]) {
+                gameOver = true;
+                return gameOver;
+            }
+        }
+
+        // check diagonals
+        if (board[0][0] === board[1][1] === board[2][2]) {
+            gameOver = true;
+            return gameOver;
+        }
+        if (board[0][2] === board[1][1] === board[2][0]) {
+            gameOver = true;
+            return gameOver;
+        }
+        return gameOver;
+        
+    }
+    return {board, isFull, markBoard, victory, gameOver}
 })();
 
 const Player = (marker) => {
@@ -54,7 +84,7 @@ function playGame(gameBoard, displayController) {
     currPlayerMessage.textContent = `Current Player: ${currPlayer.marker}`;
     const body = document.querySelector('body');
     body.appendChild(currPlayerMessage);
-    boxes = document.querySelectorAll('.box');
+    let boxes = document.querySelectorAll('.box');
     boxes.forEach((box) => {
         const boxID = box.id;
         const boxIndex = parseInt(boxID.slice(boxID.length - 1, boxID.length)) - 1;
@@ -63,8 +93,34 @@ function playGame(gameBoard, displayController) {
             displayController.updateDisplay(board);
             currPlayer = currPlayer === player1 ? player2 : player1;
             currPlayerMessage.textContent = `Current Player: ${currPlayer.marker}`;
+            if (gameBoard.victory()) {
+                // remove event listeners 
+                    boxes = document.querySelectorAll('.box');
+                    boxes.forEach((box) => {
+                        box.removeEventListener('click', () => {
+
+                        });
+                    });
+
+                // build victory modal to display
+                const victoryModal = document.getElementById('modal');
+                const modalBody = document.querySelector('.modal-body');
+                modalBody.textContent = `${currPlayer.marker} wins!`;
+                victoryModal.style.display = "block";
+                const closeButton = document.getElementsByClassName('btn-close')[0];
+                closeButton.onclick = () => {
+                    victoryModal.style.display = "none";
+                };
+                window.onclick = (event) => {
+                    if (event.target === victoryModal) {
+                        victoryModal.style.display = "none";
+                    }
+                };
+            }
         });
     });
 }
 
-playGame(gameBoard, displayController);
+if (!gameBoard.gameOver) {
+    playGame(gameBoard, displayController);
+}
