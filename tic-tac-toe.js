92bbdbdc8cf0c0963aa33fb7654f72db7334c783
Andrew <andrew.chen986@gmail.com>
2022-10-20 20:43:34 -0500

    Add random AI player

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -91,7 +91,9 @@ const Player = (name, marker, AI) => {
                 }
             }
         }
-        return vacancies;
+        const randomCoord = Math.floor(Math.random() * vacancies.length);
+        const randomIndex = (3*vacancies[randomCoord][0] + vacancies[randomCoord][1]);
+        return randomIndex;
     }
 
     return {name, marker, AI, makeMoveRandom}
@@ -102,7 +104,7 @@ const displayController = (() => {
         for (let i = 0; i < board.length; i++) {
             for (let j = 0; j < board[i].length; j++) {
                 const squareIndex = 3*i + j;
-                const targetID = `box-${squareIndex+1}`;
+                const targetID = `box-${squareIndex}`;
                 const square = document.getElementById(targetID);
                 square.textContent = board[i][j];
             }
@@ -115,8 +117,8 @@ function playGame(gameBoard, displayController) {
     let board = gameBoard.board;
     const p1Name = document.getElementsByClassName('set-player1')[0].textContent;
     const p2Name = document.getElementsByClassName('set-player2')[0].textContent;
-    const player1 = Player(p1Name, `X`, false);
-    let player2 = Player(p2Name, `O`, false);
+    const player1 = Player(p1Name, `X`, 'human');
+    let player2 = Player(p2Name, `O`, 'human');
     let currPlayer = player1;
     const currPlayerMessage = document.createElement('div');
     currPlayerMessage.textContent = `Current Player: (${currPlayer.marker}) ${currPlayer.name}`;
@@ -128,14 +130,23 @@ function playGame(gameBoard, displayController) {
 
     let easyAI = document.getElementById('easyAI');
     easyAI.addEventListener('click', () => {
-        if (player2.AI === false) {
-            player2 = Player(p2Name, 'O', true);
+        if (player2.AI !== 'random') {
+            player2 = Player(p2Name, 'O', 'random');
             easyAI.classList.add('active');
         }
         else {
-            player2 = Player(p2Name, 'O', false);
+            player2 = Player(p2Name, 'O', 'human');
             easyAI.classList.remove('active');
         }
+        let boxes = document.querySelectorAll('.box');
+        board = gameBoard.resetBoard();
+        displayController.updateDisplay(board);
+        currPlayer = player1;
+        currPlayerMessage.textContent = `Current Player: (${currPlayer.marker}) ${currPlayer.name}`;
+        boxes.forEach((box) => {
+            box.replaceWith(box.cloneNode(true));
+        });
+        initalizeBoxes();
     });
     
     let resetButton = document.getElementById('reset');
@@ -170,7 +181,7 @@ function playGame(gameBoard, displayController) {
         let boxes = document.querySelectorAll('.box');
         boxes.forEach((box) => {
             const boxID = box.id;
-            const boxIndex = parseInt(boxID.slice(boxID.length - 1, boxID.length)) - 1;
+            const boxIndex = parseInt(boxID.slice(boxID.length - 1, boxID.length));
             box.addEventListener('click', () => {
                 const validMove = gameBoard.markBoard(currPlayer, boxIndex);
                 displayController.updateDisplay(board);
@@ -180,11 +191,11 @@ function playGame(gameBoard, displayController) {
                     // build victory modal to display
                     const victoryModal = document.getElementById('modal');
                     const modalBody = document.querySelector('.modal-body');
-                    if (draw) {
-                        modalBody.textContent = `The game is a draw.`;
+                    if (gameBoard.gameOver) {
+                        modalBody.textContent = `${currPlayer.name} wins!`;
                     }
                     else {
-                        modalBody.textContent = `${currPlayer.name} wins!`;
+                        modalBody.textContent = `The game is a draw.`;
                     }
                     victoryModal.style.display = "block";
                     const closeButton = document.getElementsByClassName('btn-close')[0];
@@ -200,7 +211,41 @@ function playGame(gameBoard, displayController) {
                         box.replaceWith(box.cloneNode(true));
                     });
                 }
-                if (validMove) {
+                // make AI move if player 2 is AI
+                if (player2.AI !== 'human' && !gameBoard.gameOver) {
+                    const draw = gameBoard.isFull();
+                    if (!draw) {
+                        const boxIndex = player2.makeMoveRandom(board);
+                        gameBoard.markBoard(player2, boxIndex);
+                        displayController.updateDisplay(board);
+                        gameBoard.gameOver = gameBoard.victory();                        
+                    }
+                    if (gameBoard.gameOver || draw) {    
+                        // build victory modal to display
+                        const victoryModal = document.getElementById('modal');
+                        const modalBody = document.querySelector('.modal-body');
+                        if (gameBoard.gameOver) {
+                            modalBody.textContent = `${player2.name} wins!`;
+                        }
+                        else {
+                            modalBody.textContent = `The game is a draw.`;
+                        }
+                        victoryModal.style.display = "block";
+                        const closeButton = document.getElementsByClassName('btn-close')[0];
+                        closeButton.onclick = () => {
+                            victoryModal.style.display = "none";
+                        };
+                        window.onclick = (event) => {
+                            if (event.target === victoryModal) {
+                                victoryModal.style.display = "none";
+                            }
+                        };
+                        boxes.forEach((box) => {
+                            box.replaceWith(box.cloneNode(true));
+                        });
+                    }
+                }
+                if (validMove && player2.AI === 'human') {
                     currPlayer = currPlayer === player1 ? player2 : player1;
                 }
                 currPlayerMessage.textContent = `Current Player: (${currPlayer.marker}) ${currPlayer.name}`;
